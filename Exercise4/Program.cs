858c5ba42c5fb1aca9bbbbb5437e49e5fffe79d4
Daria <dashazoueva@gmail.com>
2022-05-16 17:07:48 +0300

    Exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+void Squares(int a)
+{
+    int i = 1;
+    
+    while (i <= a)
+    {
+        Console.WriteLine(Math.Pow(i, 2));
+        i++;
+    }
+}
+Squares(5);
