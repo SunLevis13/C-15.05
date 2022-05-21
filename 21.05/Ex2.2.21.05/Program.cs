265e2cdfbb47c7fff3c7c166fc982e6e95f91269
Daria <dashazoueva@gmail.com>
2022-05-22 00:28:28 +0300

    21.05

---
+++ 21.05/Ex2.2.21.05/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+int NumCount(int num)
+{
+    string result = num.ToString();
+    return result.Length;
+}
+
+Console.WriteLine(NumCount(456));
+Console.WriteLine(NumCount(78));
+Console.WriteLine(NumCount(89126));
