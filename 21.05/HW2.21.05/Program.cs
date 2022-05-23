7dffabb97740e031df37eef18d4b2e74f92b1da3
Daria <dashazoueva@gmail.com>
2022-05-23 12:30:19 +0300

    hw2

---
+++ 21.05/HW2.21.05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNum(int num)
+{
+    int all_sum = 0;
+    while (num > 0)
+    {
+        all_sum = all_sum + num % 10;
+        num = num / 10;
+
+    }
+    return all_sum;
+}
+Console.WriteLine(SumNum(452));
