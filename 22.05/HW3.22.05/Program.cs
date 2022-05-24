3bb4ff4479cafd0ea302f0f90e35beacc02b82ef
Daria <dashazoueva@gmail.com>
2022-05-24 16:12:08 +0300

    hw3

---
+++ 22.05/HW3.22.05/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] Massive(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(16);
+    }
+    return arr;
+}
+
+int[] MinMaxdiff(int[] arr)
+{
+    int diff = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int min = 0;
+        for (int i = 0; i < arr.Length; i++)
+
+        {
+            if (arr[i] < arr[min])
+            {
+                min = i;
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < arr.Length; i++)
+
+        {
+            if (arr[i] > arr[max])
+            {
+                max = i;
+            }
+        }
+        return arr;
+    }
+    diff = diff + arr[min] + arr[max];
+    return diff;
+
+}
+
+
+int[] arr_1 = Massive(9);
+MinMaxdiff(arr_1);
+Print(arr_1);
+
+
