21c6be9bd88503bc8c0e70d0e954d79bc63e7ac1
Daria <dashazoueva@gmail.com>
2022-05-30 11:35:38 +0300

    hw2

---
+++ 28.05/HW2.28.05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+// b1, k1, b2 и k2 задаются пользователем.
+
+void Cross(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Cross parameter: ({x}; {y})");
+}
+
+Cross(1, 6, 4, 8);
