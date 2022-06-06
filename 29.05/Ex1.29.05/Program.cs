d5c7a40c82be4519648f68b7460944dad25c64a5
Daria <dashazoueva@gmail.com>
2022-06-06 14:54:49 +0300

    ex1

---
+++ 29.05/Ex1.29.05/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        } 
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {   
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }        
+    }
+    return arr;
+}
+
+Console.Write("Введите количество строк row: ");
+int row = int.Parse (Console.ReadLine());
+
+Console.Write("Введите количество столбцов column: ");
+int column = int.Parse (Console.ReadLine());
+
+Print(MassNums(row,column,10,21));
