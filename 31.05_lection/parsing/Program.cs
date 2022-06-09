489357bfd6354835e5775d9985fd607482743257
Daria <dashazoueva@gmail.com>
2022-06-09 13:46:21 +0300

    pars

--- 31.05_lection/parsing/Program.cs
+++ 31.05_lection/parsing/Program.cs
@@ -2,19 +2,19 @@
 
 using System.Linq;
 
-string text = "(1,2) (2,3) (4,5) (6,7)"
-        .Replace ("(", "")
+string text = "(1,2) (2,3) (4,5) (6,7)" // задаем координаты каких то точек
+        .Replace ("(", "") // убираем скобки
         .Replace (")", "")
         ;
 Console.WriteLine (text);
 
-var data = text.Split (" ")
-        .Select (item => item.Split (','))
-        .Select (e => (x: int.Parse (e[0]), y: int.Parse (e[1])))
-        .ToArray();
+var data = text.Split (" ") // задаем разделитель, в качестве разделителя пробел
+        .Select (item => item.Split (',')) // выбрать элементы для которых надо будет новое разбиение через запятую
+        .Select (e => (x: int.Parse (e[0]), y: int.Parse (e[1]))) // сделать выборку из текущего массива такого, что 1ая координата
+        //будет взята как 1ый эл-т массива и сразу превращена в число. И то же самое со 2ой
+        .ToArray(); // вывод в массив
 
 for (int i=0; i < data.Length; i++)
-
 {
-		Console.WriteLine (data[i].x*10 );
+		Console.WriteLine (data[i].x*10 ); // вывод точка массива с координатой х умноженной на 10
 }
