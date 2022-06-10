c561578f2f8379fc734eca53e6736c64906dcffc
Daria <dashazoueva@gmail.com>
2022-06-10 11:27:50 +0300

    ex5

---
+++ 07.06_lection/Ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Факториал числа
+
+// обычный метод
+int PowerFor(int a, int n)
+{ int result = 1;
+ for (int i = 1; i <= n; i++) result *= a;
+ return result;
+}
+
+// метод Рекурсии
+int PowerRec(int a, int n)
+{ //return n == 0 ? 1 : PowerRec(a, n - 1) * a; // можно записать одной строкой
+ if (n == 0) return 1;
+ else return PowerRec(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
