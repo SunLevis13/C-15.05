0a7fe78d7ddef8dba6348d08c5f0ab62f8003332
Daria <dashazoueva@gmail.com>
2022-06-10 11:11:53 +0300

    ex3

---
+++ 07.06_lection/Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Сумма чисел от 1 до n
+
+// обычный метод
+int SumFor(int n)
+{
+ int result = 0;
+ for (int i = 1; i <= n; i++) result += i;
+ return result;
+}
+Console.WriteLine(SumFor(10)); // 55
+
+// метод Рекурсии
+int SumRec(int n)
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+
+Console.WriteLine(SumRec(10)); // 55
