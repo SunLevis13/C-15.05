5534cadfa379c9e3f7990118f3b3b35240fd1880
Daria <dashazoueva@gmail.com>
2022-06-10 11:45:31 +0300

    ex5

--- 07.06_lection/Ex5/Program.cs
+++ 07.06_lection/Ex5/Program.cs
@@ -16,3 +16,13 @@ int PowerRec(int a, int n)
 
 Console.WriteLine(PowerFor(2, 10)); // 1024
 Console.WriteLine(PowerRec(2, 10)); // 1024
+
+// 3ий способ с испльзованием формул и рекурсии
+int PowerRecMath(int a, int n)
+{
+ if (n == 0) return 1;
+ else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+ else return PowerRecMath(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerRecMath(2, 10)); // 1024
