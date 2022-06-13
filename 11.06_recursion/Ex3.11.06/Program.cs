ae0a24eb6d4712c033e24be448517d6897f7e564
Daria <dashazoueva@gmail.com>
2022-06-13 17:33:34 +0300

    ex3

---
+++ 11.06_recursion/Ex3.11.06/Program.cs
@@ -0,0 +1,18 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в».
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+char[] massiv = { 'а', 'и', 'с', 'в' };
+int count = 2;
+
+void FindWords(char[] search, char[] dict, int n = 0)
+{
+    if (n == dict.Length)
+    {
+        Console.WriteLine(new String(dict)); return;
+    }
+    for (int i = 0; i < search.Length; i++)
+    {
+        dict[n] = search[i];
+        FindWords(search, dict, n + 1);
+    }
+}
+FindWords(massiv, new char[count]);
