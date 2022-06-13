992873281b3dd64d926a91f4b0d725d749463e10
Daria <dashazoueva@gmail.com>
2022-06-13 16:46:16 +0300

    ex2

--- 11.06_recursion/Ex2.11.06/Program.cs
+++ 11.06_recursion/Ex2.11.06/Program.cs
@@ -5,7 +5,8 @@ int Value (int num1, int num2, int n)
 {
     if (n == 1) return num1;
     Console.Write ($"{num1} ");
-    (num1,num2) = (num2,num2 + num1);
-    return Value (num1,num2,n-1);
+    // (num1,num2) = (num2,num2 + num1);
+    //return Value (num1,num2,n-1);
+    return Value (num2,num2 + num1,n-1); // можно заменить одной строкой две выше
 }
 Console.WriteLine(Value(3,4,5));
