7026d88f4bab4ae601f587a869b2dc20fed7de0c
Daria <dashazoueva@gmail.com>
2022-06-13 09:57:06 +0300

    ex8

---
+++ 07.06_lection/Ex8/Program.cs
@@ -0,0 +1,32 @@
+﻿// Рекурсия для хождения по папкам и смотреть что внутри
+void CatalogInfo(string path, string indent = "")
+
+{
+
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+
+
+    foreach (var currentCatalog in catalogs.GetDirectories())
+
+    {
+
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+
+        CatalogInfo(currentCatalog.FullName, indent + "  ");
+
+    }
+
+
+    foreach (var item in catalogs.GetFiles())
+
+    {
+
+        Console.WriteLine($"{indent}{item.Name}");
+
+    }
+
+}
+
+string path = @"/Users/dariazueva/Dropbox/Zueva/GB";
+
+CatalogInfo(path);
