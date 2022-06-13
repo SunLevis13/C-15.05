9ebc785864ef7bc98001f697f60d07dadd55df90
Daria <dashazoueva@gmail.com>
2022-06-13 16:42:04 +0300

    ex2

---
+++ 11.06_recursion/Ex2.11.06/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел,
+// для которых каждое следующее равно сумме двух предыдущих. 
+
+int Value (int num1, int num2, int n)
+{
+    if (n == 1) return num1;
+    Console.Write ($"{num1} ");
+    (num1,num2) = (num2,num2 + num1);
+    return Value (num1,num2,n-1);
+}
+Console.WriteLine(Value(3,4,5));
