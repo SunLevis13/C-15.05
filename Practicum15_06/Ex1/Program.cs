be029dd8b429fb24539852858b18ebc59fd3f78e
Daria <dashazoueva@gmail.com>
2022-06-30 13:28:10 +0300

    ex1

--- Practicum15_06/Ex1/Program.cs
+++ Practicum15_06/Ex1/Program.cs
@@ -1,1 +1,28 @@
 ﻿// Задан массив. Задать новый массив, где все элементы четные
+void methodArray (string [] array)
+{
+     string[] newarr = new string[array.Length];
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3)
+        {
+        newarr[i]=array[i];
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
+void Print(string [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+    Console.WriteLine();
+}
+
+string [] array = {"hello", "2", "world", ":-)"};
+
+methodArray(array);
+Print(array);
