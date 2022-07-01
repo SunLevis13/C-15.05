769b276cc8130c1a014c988e6a38ca831bb051e0
Daria <dashazoueva@gmail.com>
2022-07-01 17:48:32 +0300

    task1

---
+++ TASK_control_05.07/task/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string[] array = {"hello", "2", "world", ":-)"};
+string[] new_array = new string[array.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void NewArray(string[] array, string[] new_array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        new_array[j] = array[i];
+        j++;
+        }
+    }
+   
+}
+Console.WriteLine();
+
+NewArray(array, new_array);
+PrintArray(new_array);
