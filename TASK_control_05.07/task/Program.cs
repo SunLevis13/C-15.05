cf309fd27791d9de875c5d34b26b11bf6874b04f
Daria <dashazoueva@gmail.com>
2022-07-01 18:24:15 +0300

    task2

--- TASK_control_05.07/task/Program.cs
+++ TASK_control_05.07/task/Program.cs
@@ -1,30 +1,26 @@
-﻿
-string[] array = {"hello", "2", "world", ":-)"};
-string[] new_array = new string[array.Length];
-
-void PrintArray(string[] array)
+﻿// string[] array = {"hello", "2", "world", ":-)"};
+// string[] new_array = new string[array.Length];
+void NewArray(string [] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-
-void NewArray(string[] array, string[] new_array)
-{
-    int j = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-    if(array[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-        new_array[j] = array[i];
-        j++;
+            Console.Write($"{array[i]} ");
         }
     }
-   
 }
-Console.WriteLine();
 
-NewArray(array, new_array);
-PrintArray(new_array);
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"new array is ");
+    }
+    
+}
+
+
+string [] array_1 = {"hello", "2", "world", ":-)"};
+NewArray(array_1);
+PrintArray(array_1);
