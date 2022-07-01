08a02ebb523b9876e2accd2edb28a6d3c4fba416
Daria <dashazoueva@gmail.com>
2022-07-01 17:24:43 +0300

    Ex1done

--- Practicum15_06/Ex1/Program.cs
+++ Practicum15_06/Ex1/Program.cs
@@ -1,28 +1,59 @@
 ﻿// Задан массив. Задать новый массив, где все элементы четные
-void methodArray (string [] array)
-{
-     string[] newarr = new string[array.Length];
-     for (int i=0; i<array.Length; i++)
-     {      
-        if(array[i].Length<=3)
-        {
-        newarr[i]=array[i];
-        Console.WriteLine(newarr[i]); 
-        }
-     }
-        Console.WriteLine(); 
-}
+// void methodArray (string [] array)
+// {
+//      string[] newarr = new string[array.Length];
+//      for (int i=0; i<array.Length; i++)
+//      {      
+//         if(array[i].Length<=3)
+//         {
+//         newarr[i]=array[i];
+//         Console.Write("newarr = ");
+        
+//         }Console.Write($"{newarr[i]} ");
+//            }
+//         Console.WriteLine();
+// }
 
-void Print(string [] array)
+// void Print(string [] array)
+// {
+//     for (int i=0; i<array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// string [] array = {"hello", "2", "world", ":-)"};
+
+// methodArray(array);
+// Print(array);
+
+
+string[] array = {"123", "23", "hello", "world", "res"};
+string[] new_array = new string[array.Length];
+
+void PrintArray(string[] array)
 {
-    for (int i=0; i<array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine(array[i]);
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
 
-string [] array = {"hello", "2", "world", ":-)"};
+void SecondArray(string[] array, string[] new_array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(i <= 2)
+        {
+        new_array[j] = array[i];
+        j++;
+        }
+    }
+}
 
-methodArray(array);
-Print(array);
+
+SecondArray(array, new_array);
+PrintArray(new_array);
