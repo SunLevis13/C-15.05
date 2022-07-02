93c0e5bc4d36eccf25ca7044e474f37efd028d3a
Daria <dashazoueva@gmail.com>
2022-07-02 22:45:08 +0300

    task last

--- TASK_control_05.07/task/Program.cs
+++ TASK_control_05.07/task/Program.cs
@@ -1,23 +1,61 @@
 ﻿
+// void NewArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             Console.Write($"{array[i]} ");
+//         }
+
+//     }
+// }
+
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("New array is: ");
+//         break;
+//     }
+// }
+
+// string[] array_1 = { "hello", "2", "world", ":-)" };
+// PrintArray(array_1);
+// NewArray(array_1);
+// Console.WriteLine();
+
+// string[] array_2 = { "1234", "1567", "-2", "computer science" };
+// PrintArray(array_2);
+// NewArray(array_2);
+// Console.WriteLine();
+
+// string[] array_3 = { "Russia", "Denmark", "Kazan" };
+// PrintArray(array_3);
+// NewArray(array_3);
+
 void NewArray(string[] array)
 {
+    string[] newarray = new string[array.Length];
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            Console.Write($"{array[i]} ");
+            newarray[i] = array[i];
         }
-
+        Console.Write($"{newarray[i]} ");
     }
+
 }
 
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write("New array is: ");
+        Console.Write("New Array =");
         break;
     }
+
 }
 
 string[] array_1 = { "hello", "2", "world", ":-)" };
@@ -33,5 +71,3 @@ Console.WriteLine();
 string[] array_3 = { "Russia", "Denmark", "Kazan" };
 PrintArray(array_3);
 NewArray(array_3);
-
-
